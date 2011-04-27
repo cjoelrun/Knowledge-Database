a0f33714b28171c7f7921b6f6b6d2e590e06769b
Cameron Lopez <cameron@cameron-macbook.local>
2011-04-27 17:02:27 -0500

    added search as separation

--- knowledge.scm
+++ knowledge.scm
@@ -1,32 +1,20 @@
-(define logic '())
-(set! logic (cons '(object '(name "Jody")) logic))
+(define atom? (lambda (x) (or (number? x) (symbol? x) (boolean? x))))
 
-(define add_replace (lambda (item)
-			(if car
-
-(define parse (lambda (item)
-		(set! knowledge (cons
-				 (object (name (car item)))
-				 (description (cdr (cdr item))) logic))))
 (define knowledge '())
 (define save (lambda (item)
-               (set! knowledge (cons item knowledge))))
-
-(define query (lambda (question)
-                knowledge))
-;; (Object (name ""))
-
-;; (relationship (description "")
-;; 	      (connection '())
-
-'("Jody")
-        '(description "an actress")
-        '(description "a dancer")
-
-
-;; (actress (Object)
-;; 	 (name "actress"))
-
-;; (is (relationship)
-;;     (decription "is")
-;;     (connection (Jody actress)))
+	       (set! logic
+		     (cons (cons
+			    (car item)
+			    (cdr (cdr item)))
+			   logic))))
+(define print (lambda (subject) 0))
+(define search (lambda (question subject)
+                 (cond ((atom? question) "Usage: (query '(<subject> is <description>")
+                       ((null? question) "Usage: (query '(<subject> is <description>")
+                       ((else (if (equal? (car question) "is") (if (empty? subject) "Usage: (query '(<subject> is <description>" 
+                                                                   (print subject))
+                                  (search question)
+				  
+				  
+				  (define query (lambda (question)
+						  (search question '())))
